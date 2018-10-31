84a2d0c08df0256a7ae00df0f48ea2a615984c71
trujic1000 <aleksandar.trujic@live.com>
2018-10-30 22:44:29 -0500

    user model email verification added

--- server/user/userController.js
+++ server/user/userController.js
@@ -1,15 +1,14 @@
 const { User } = require('./userModel');
 
+const _ = require('lodash');
+
 function signup(req, res) {
-  const { email, password } = req.body;
-  const newUser = new User({
-    email,
-    password
-  });
+  const body = _.pick(req.body, ['email', 'password']);
+  const newUser = new User(body);
   (async function saveUser(){
     try {
       const savedUser = await newUser.save();
-      res.status(200).json(savedUser, undefined, 2);
+      res.status(200).send({savedUser});
     } catch (error) {
       res.status(400).send(error);
     }
--- server/user/userModel.js
+++ server/user/userModel.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
+const validator = require('validator');
+
 const UserSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
   },
   password: {
     type: String,
